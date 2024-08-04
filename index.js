d60470e12b1d7c94260e10c9ac8752692b9ea42a
rgonzales4 <roshel.gonzales@logilica.com>
2024-08-04 22:18:10 +1000

    feat: :sparkles: completed basic functionality of the game
    all pairs are iterated and rendered (images and labels), reformatted data structure

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 // TODO: I COULD paste the data in this file rather than reading from a separate JSON file?
+/* 
+	Data structure: 
+	{
+		"mainCategory": {
+			"subCategory" : {
+				"option1" : linkToImg,
+				"option2" : linkToImg,
+				...
+			},
+			...
+		},
+		...
+	}
+*/
 
 // fetchJSONData() is purely to render category options + redirect user to the correct page, depending on chosen categories
 function fetchJSONData() {
@@ -108,8 +122,6 @@ function createPairs() {
 			return res.json();
 		})
 		.then((data) => {
-			console.log("Creating pairs...");
-
 			// On some reload - If a main + sub category is selected, redirect user to games page (if they're not in it already)
 			const mainCategory = localStorage.getItem("mainCategory");
 			const subCategory = localStorage.getItem("subCategory");
@@ -118,81 +130,140 @@ function createPairs() {
 			if (!mainCategory || !subCategory) {
 				console.log("Categories hasn't been chosen. Redirecting to homepage", { mainCategory, subCategory });
 				window.location.pathname = "/index.html";
-			}
-
-			// Get options
-			const options = data[mainCategory][subCategory];
-			if (!options) {
-				console.log("No options found", { mainCategory, subCategory });
-				backToHome();
 				return;
 			}
 
-			// TODO: check if options have been formatted - load them instead of remaking options...
+			// If there's no pairs, create pairs
+			if (!localStorage.getItem("pairs")) {
+				console.log("Creating pairs.");
 
-			const pairs = [];
-			// Create random pairs, non-repeating
-			if (options.length % 2 != 0) {
-				alert("You must have an even number of options. You currently have " + options.length + " options.");
-				backToHome();
-				return;
-			}
-
-			// Create arr to keep track of which options have been used already
-			const seenOptions = [];
-
-			// Copy and shuffle the options
-			const shuffledOptions = options.slice();
-			shuffledOptions.sort(() => 0.5 - Math.random()); // shuffle arrays
-			console.log(shuffledOptions);
-
-			// Create pairs
-			for (const o of shuffledOptions) {
-				// Check if options has been used already
-				if (seenOptions.find((f) => f === o)) {
-					continue;
+				// Get options
+				const options = Object.keys(data[mainCategory][subCategory]);
+				console.log(options);
+				if (!options) {
+					console.log("No options found", { mainCategory, subCategory });
+					backToHome();
+					return;
 				}
-				seenOptions.push(o);
 
-				// Get pair value
-				let pairVal = undefined;
-				while (pairVal === undefined) {
-					// Get a random option
-					const randomIndex = [Math.floor(Math.random() * shuffledOptions.length)];
-					const randomOpt = shuffledOptions[randomIndex];
-					if (seenOptions.find((f) => f === randomOpt)) {
+				// TODO: check if options have been formatted - load them instead of remaking options...
+
+				const pairs = [];
+				// Create random pairs, non-repeating
+				if (options.length % 2 != 0) {
+					alert("You must have an even number of options. You currently have " + options.length + " options.");
+					backToHome();
+					return;
+				}
+
+				// Create arr to keep track of which options have been used already
+				const seenOptions = [];
+
+				// Copy and shuffle the options
+				const shuffledOptions = options.slice();
+				shuffledOptions.sort(() => 0.5 - Math.random()); // shuffle arrays
+
+				// Create pairs
+				for (const o of shuffledOptions) {
+					// Check if options has been used already
+					if (seenOptions.find((f) => f === o)) {
 						continue;
 					}
-					pairVal = randomOpt;
-					seenOptions.push(randomOpt);
-					// TODO: Remove the option from shuffledOptions? to optimise logic - not needed tho
+					seenOptions.push(o);
+
+					// Get pair value
+					let pairVal = undefined;
+					while (pairVal === undefined) {
+						// Get a random option
+						const randomIndex = [Math.floor(Math.random() * shuffledOptions.length)];
+						const randomOpt = shuffledOptions[randomIndex];
+						if (seenOptions.find((f) => f === randomOpt)) {
+							continue;
+						}
+						pairVal = randomOpt;
+						seenOptions.push(randomOpt);
+						// TODO: Remove the option from shuffledOptions? to optimise logic - not needed tho
+					}
+
+					// Store pair
+					pairs.push([o, pairVal]);
 				}
 
-				// Store pair
-				pairs.push([o, pairVal]);
+				console.log("Saving pairs", pairs);
+				localStorage.setItem("pairs", JSON.stringify(pairs));
 			}
 
-			console.log(pairs);
+			gameLogic(data, mainCategory, subCategory);
+
+			// const localStoragePairs = localStorage.getItem("pairs");
+			// console.log("localStoragePairs: ", localStoragePairs);
 		})
 		.catch((error) => console.error("Unable to create pairs", error));
 }
 
-function gameLogic() {
-	console.log("Game Logic!");
-	// TODO: load in pairs into UI
-	// use localStorage to keep track of which pair is to be shown
-	// onClick listeners to change the pairs shown + animation on the card that was clicked?
+function gameLogic(data, mainCategory, subCategory) {
+	/* 
+		At this point, it's safe to assume that the pairs have been created bc this func is ONLY
+		called from createPairs().  
+	*/
+
+	// Make sure the user is in the correct page (game.html).
+	const gameTitle = document.getElementById("gameTitle");
+	if (!gameTitle) {
+		console.log("Attempting to load Game Logic, but it appears the user is in the wrong page.");
+		backToHome();
+	}
+
+	// Get pairs
+	const localStoragePairs = localStorage.getItem("pairs");
+	const parsedPairs = JSON.parse(localStoragePairs) || [];
+
+	// currentIndex keeps track of which pair to show
+	const currentIndex = localStorage.getItem("currentIndex") || 0;
+	console.log({ pairs: parsedPairs, currentIndex });
+
+	// If currentIndex is greater than the number of items = end of game.
+	if (currentIndex > parsedPairs.length - 1) {
+		console.log("End of pairs.");
+		document.getElementById("gameDiv").style.display = "none";
+		document.getElementById("endOfGameDiv").style.display = "flex";
+	}
+
+	// Get current slide
+	const options = parsedPairs[currentIndex];
+	console.log("Current Slide:", options);
+
+	// Display labels + images
+	const option1 = options[0],
+		option2 = options[1];
+	// Labels
+	document.getElementById("topCardLabel").innerHTML = option1;
+	document.getElementById("bottomCardLabel").innerHTML = option2;
+	// Images
+	document.getElementById("topCardImg").src = data[mainCategory][subCategory][option1];
+	document.getElementById("bottomCardImg").src = data[mainCategory][subCategory][option2];
+
+	// Update pairs list + Current index tracker
+	localStorage.setItem("pairs", JSON.stringify(parsedPairs));
+	localStorage.setItem("currentIndex", currentIndex);
+}
+
+function nextIndex() {
+	let currentIndex = JSON.parse(localStorage.getItem("currentIndex")) || 0;
+	localStorage.setItem("currentIndex", currentIndex + 1);
+	window.location.reload();
 }
 
 document.getElementById("topCard").addEventListener("click", () => {
 	console.log("topCard");
+	nextIndex();
 });
 
 document.getElementById("bottomCard").addEventListener("click", () => {
 	console.log("bottomCard");
+	nextIndex();
 });
 
 if (window.location.pathname.includes("game.html")) {
 	createPairs();
-	gameLogic();
 }
